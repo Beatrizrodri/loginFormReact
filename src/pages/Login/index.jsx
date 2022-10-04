3191fda5843b51f6f0ae4b91ab649a1fe013675f
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-04 19:03:07 -0300

    add login page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+import '../../styles/global.css'
+
+export default function Login() {
+  return (
+    <div id="container">
+      <div id="background"></div>
+      <div id="image-container"></div>
+    </div>
+  )
+}
