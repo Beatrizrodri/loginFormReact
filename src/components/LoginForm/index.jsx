22132e87e06aeb942e9f742ae4b36fa5fde25401
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-05 22:44:52 -0300

    add login form component

---
+++ src/components/LoginForm/index.jsx
@@ -0,0 +1,13 @@
+import Input from '../Input'
+import Title from '../Title'
+import './styles.css'
+
+export default function LoginForm() {
+  return (
+    <div id="container-login-form">
+      <Title />
+      <Input label="E-mail" type="email" placeholder="Digite seu email" />
+      <Input label="Senha" type="password" placeholder="Digite sua senha" />
+    </div>
+  )
+}
