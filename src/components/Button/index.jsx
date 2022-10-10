54930b4ee2d589679dc3e2845bcd5b68485969fa
Beatriz Rodrigues <beatrizrodri37@gmail.com>
2022-10-10 10:54:33 -0300

    add button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+export default function Button() {
+  return (
+    <div id="button-content">
+      <button>Entrar</button>
+      <span>
+        Não tem uma conta? <a href="">Registre-se</a>
+      </span>
+    </div>
+  )
+}
